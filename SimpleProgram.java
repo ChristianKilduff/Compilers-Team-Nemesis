6bf08ff6413ce8114f50b0845754e459fec9426f
JeremyGerm <jeremyrwiening@gmail.com>
2025-12-09 23:03:08 -0500

    problem4 compiles besides line 48

--- SimpleProgram.java
+++ SimpleProgram.java
@@ -3,42 +3,46 @@ public class SimpleProgram {
 static Scanner ___protected___in___ = new Scanner(System.in);
 public static void main(String[] args) throws Exception {
 int n=0;
-System.out.println("input number floats to read: ");
+System.out.println("input the array size");
 n=___protected___in___.nextInt();
+while(n<=0) {
+System.out.println("Array size n must be positive. Try again");
+n=___protected___in___.nextInt();
+}
 ArrayList<Double> listA= new ArrayList<Double>();
 Collections.addAll(listA, new Double[]{0.0});
-ArrayList<Double> listB= new ArrayList<Double>();
-Collections.addAll(listB, new Double[]{0.0});
 listA.clear();
-listB.clear();
-double mean=0.0;
-System.out.println("input number floats: ");
-for (int ____protected_index____1 = 0; ____protected_index____1 < n; ____protected_index____1++) {
-double in=0.0;
-in=___protected___in___.nextDouble();
-listA.add(in);
-mean=mean + in;
-}
-mean=mean / n;
 int i=0;
-for (int ____protected_index____1 = 0; ____protected_index____1 < n; ____protected_index____1++) {
-double value=listA.get(i);
-value=value - mean;
-listB.add(value);
+while(i<n) {
+temp=___protected___in___.nextInt();
+listA.add(temp);
 i=i + 1;
 }
-System.out.println("---");
+double total=listA.get(-1);
+double sum=0.0;
+double min=listA.get(-1);
+double max=listA.get(-1);
+i=1;
+while(i<n) {
+ sum=listA.get(i);
+total=total + sum;
+if(sum<min)
+{
+sum=min;
+}
+if(sum>max)
+{
+sum=max;
+}
+i=i + 1;
+}
+double mean=total;
+int above_mean=0;
 i=0;
-for (int ____protected_index____1 = 0; ____protected_index____1 < n; ____protected_index____1++) {
-double v=listB.get(i);
-System.out.println(v);
-i=i + 1;
+while(i<n) {
+if(listAnulli)
+above_mean=above_mean + 1;
 }
-System.out.println("");
-System.out.println("mean: ");
-System.out.println("list A: ");
-System.out.println(listA);
-System.out.println("list B: ");
-System.out.println(listB);
+i=i + 1;
 }
 }
